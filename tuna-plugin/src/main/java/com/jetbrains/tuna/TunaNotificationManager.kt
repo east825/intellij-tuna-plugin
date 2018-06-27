0836eb94ddf436af858e079942a841dc5b440570
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2018-06-27 19:30:02 +0300

    Add support for branch checkout and indexing

--- tuna-plugin/src/main/java/com/jetbrains/tuna/TunaNotificationManager.kt
+++ tuna-plugin/src/main/java/com/jetbrains/tuna/TunaNotificationManager.kt
@@ -1,12 +1,14 @@
 package com.jetbrains.tuna
 
+
 import com.intellij.execution.ExecutionListener
 import com.intellij.execution.ExecutionManager
 import com.intellij.execution.process.ProcessHandler
 import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vcs.ProjectLevelVcsManager
-import com.intellij.openapi.vcs.VcsListener
+import com.intellij.openapi.vcs.BranchChangeListener
+
 
 data class TunaNotification(val title: String, val text: String)
 
@@ -16,16 +18,38 @@ class TunaNotificationManager(private val project: Project) {
     fun initProjectListeners() {
         val connection = project.messageBus.connect()
 
-        connection.subscribe(ProjectLevelVcsManager.VCS_CONFIGURATION_CHANGED, VcsListener {
-            addNotification(TunaNotification("Something changed", "in VCS"))
-        })
-
-        // use for tests, but doesn't work for Rebuild
+        // Running Run configurations
         connection.subscribe(ExecutionManager.EXECUTION_TOPIC, object : ExecutionListener {
             override fun processTerminated(executorId: String, env: ExecutionEnvironment, handler: ProcessHandler,
                                            exitCode: Int) {
-                addNotification(TunaNotification(executorId, "The process is finished with exit code {}"
-                        .format(exitCode)))
+                addNotification(TunaNotification("Process $env finished with exit code $exitCode", ""))
+            }
+        })
+
+//        connection.subscribe(GitRepository.GIT_REPO_CHANGE, object: GitRepositoryChangeListener {
+//            override fun repositoryChanged(rep: GitRepository) {
+//                addNotification(TunaNotification(rep.info.toString(), "in git repository"))
+//            }
+//        })
+
+        // Successful branch check out
+        connection.subscribe(BranchChangeListener.VCS_BRANCH_CHANGED, object : BranchChangeListener {
+            override fun branchWillChange(branchName: String) {
+            }
+
+            override fun branchHasChanged(branchName: String) {
+                addNotification(TunaNotification("Branch %s checked out".format(branchName), ""))
+            }
+        })
+
+        // Indexing
+        connection.subscribe(DumbService.DUMB_MODE, object : DumbService.DumbModeListener {
+            override fun enteredDumbMode() {
+                addNotification(TunaNotification("Indexing started", ""))
+            }
+
+            override fun exitDumbMode() {
+                addNotification(TunaNotification("Indexing finished", ""))
             }
         })
     }
