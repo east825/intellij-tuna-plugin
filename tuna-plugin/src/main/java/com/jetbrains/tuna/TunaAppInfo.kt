3ec32918bd708ae60520bdc32c1209387e1ba7e7
Mikhail Golubev <qsolo825@gmail.com>
2018-06-29 17:12:27 +0300

    Register application in JetBrains workspace
    Don't keep client_id directly in sources instead loading it from
    an excluded properties file.

--- tuna-plugin/src/main/java/com/jetbrains/tuna/TunaAppInfo.kt
+++ tuna-plugin/src/main/java/com/jetbrains/tuna/TunaAppInfo.kt
@@ -1,10 +1,22 @@
 package com.jetbrains.tuna
 
+import java.util.*
+
 object TunaAppInfo {
-  const val CLIENT_ID = "387915614917.388681237990"
-  const val CLIENT_SECRET = "3dbdbd7210ef6d86f76a155c6bba8cb6"
+  val CLIENT_ID by lazy { loadCredentials("tuna.slack.app.client.id") }
+  val CLIENT_SECRET by lazy { loadCredentials("tuna.slack.app.client.secret") }
+
+  private fun loadCredentials(key: String): String {
+    System.getProperty(key)?.let { return it }
+
+    val storage = Properties()
+    javaClass.getResourceAsStream("/credentials.properties").use {
+      storage.load(it)
+    }
+    return storage.getProperty(key)
+  }
 
   const val OAUTH_AUTHORIZE_URL = "https://slack.com/oauth/authorize"
   const val OAUTH_ACCESS_TOKEN_URL = "https://slack.com/api/oauth.access"
-  const val REDIRECT_URI: String = "http://localhost:8000/oauth/callback"
+  const val REDIRECT_URI = "http://localhost:8000/oauth/callback"
 }
